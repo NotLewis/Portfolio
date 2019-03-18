23028e5c4b736205cd3081be36a52c46e3619079
Lewis Hall <dev.lewishall@gmail.com>
2019-03-18 21:47:26 +0000

    Added cache
    offline support

--- sw.js
+++ sw.js
@@ -1,11 +1,73 @@
 
 //Service Worker
 
+const pwaCache ='pwa-cache-11';
+
+self.addEventListener('install', (e) => {
+
+    let cacheReady = caches.open(pwaCache).then((cache) => {
+
+        console.log('New Cache Ready')
+        return cache.addAll([
+            '/',
+            'blackpoolsvg.svg',
+            'main.js',
+            'sw.js',
+            'css/style.css',
+            'pages/blog-industry.html',
+            'pages/blog-optimisation.html',
+            'pages/blog-portfolio.html'
+        ])
+    })
+
+    e.waitUntil(cacheReady)
+});
+
+
+self.addEventListener('activate', (e) => {
+
+   let cacheCleaned = caches.keys().then((keys) => {
+        keys.forEach((key) => {
+            if(key !== pwaCache) return caches.delete(key);
+        })
+    })
+
+    e.waitUntil(cacheCleaned);
+})
+
+self.addEventListener('fetch', (e) => {
+
+    // SKip for remote fetch
+    if (!e.request.url.match(location.origin) ) return;
+
+    // Serve local fetch from cache
+    let newRes = caches.open(pwaCache).then((cache) => {
+        return cache.match(e.request).then((res) => {
+        
+        // Check request was found in cache
+        if (res) {
+            console.log(`Serving ${res.url} from cache.`);
+            return res;
+        }
+
+        //Fetch on behalf of client and cache
+        return fetch(e.request).then((fetchRes) => {
+
+            cache.put(e.request, fetchRes.clone());
+            return fetchRes;
+        });
+    });
+});
+
+e.respondWith(newRes);
+
+});
+
 
 //Send notification on push
-self.addEventListener('push', (e) => {
+// self.addEventListener('push', (e) => {
 
-    let n = self.registration.showNotification('A notification from the SW.')
-    e.waitUntil(n);
-})
+//     let n = self.registration.showNotification('A notification from the SW.')
+//     e.waitUntil(n);
+// })
 
